a22cf00ba5201af5642b14e0432d0808dc77f953
Ed <edward.syasin@gmail.com>
2021-11-21 06:37:54 +0300

    Base setup

---
+++ src/router/routes.js
@@ -0,0 +1,8 @@
+import MainPage from "@/views/MainPage";
+
+export default [
+	{
+		path: '/',
+		component: MainPage
+	}
+]
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+
+module.exports = {
+	entry: __dirname + "/src/app.js",
+	output: {
+		path: __dirname + "/dist",
+		filename: 'bundle.js',
+		chunkFilename: "[id].js"
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: __dirname + "/public/index.html",
+			inject: 'body'
+		}),
+		new VueLoaderPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: "vue-loader"
+			},
+			{
+				test: /\.(s*)css/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.vue'],
+		alias: {
+			"@": __dirname + "/src"
+		}
+	},
+	devServer: {
+		static: {
+			directory: __dirname + '/public'
+		},
+		port: 7700
+	}
+}
